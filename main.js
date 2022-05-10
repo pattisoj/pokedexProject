ffd114997227feb46513787f6fae45de38b3a9bb
pattisoj <joshpattison23@icloud.com>
2022-05-10 22:40:25 +0100

    Created basic HTML. Created a random fetch for page testing. Split the webpage into two with CSS and centered the random sprite.

---
+++ main.js
@@ -0,0 +1,29 @@
+//Initial API investigation
+console.log("main.js is running...");
+
+//Creates a random number for the Pokedex ID
+const randomInteger = Math.floor(Math.random() * 151) + 1;
+console.log(randomInteger);
+
+//Pulls the name (capitalizes the first letter) and front sprite
+async function pokemonAPI() {
+  //Connects with the API
+  let response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${randomInteger}/`
+  );
+  let data = await response.json();
+  console.log(data);
+  //Takes the name data and capitalizes the first letter
+  let userNameSplit = Object.assign([], data.name);
+  userNameSplit[0] = userNameSplit[0].toUpperCase();
+  let userName = userNameSplit.join("");
+  //Takes the name data and displays it on screen
+  let h1 = document.getElementById("pokeName");
+  h1.innerText = userName;
+  //Gets the front sprite and displays it on screen
+  let imgTest = document.getElementById("pokeSprite");
+  let imgStore = data.sprites.front_default;
+  imgTest.src = imgStore;
+}
+
+pokemonAPI();
